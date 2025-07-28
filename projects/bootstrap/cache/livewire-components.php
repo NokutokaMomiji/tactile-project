fc323a766db76ebc70b2f993dbf04f37d0d871dc
NokutokaMomiji <futuretechstudios88@gmail.com>
2025-07-28 13:29:18 +0200

    Removed duplicated project folder.

--- projects/bootstrap/cache/livewire-components.php
+++
@@ -1,8 +0,0 @@
-<?php return array (
-  'feed' => 'App\\Http\\Livewire\\Feed',
-  'new-post' => 'App\\Http\\Livewire\\NewPost',
-  'post-edit' => 'App\\Http\\Livewire\\PostEdit',
-  'post-page' => 'App\\Http\\Livewire\\PostPage',
-  'upload-images' => 'App\\Http\\Livewire\\UploadImages',
-  'user-list' => 'App\\Http\\Livewire\\UserList',
-);
